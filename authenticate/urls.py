5ce8ac7880ccc87d8b20d9489eb733517caa90b0
vishal4720 <vishalpatole2000@gmail.com>
2025-02-08 00:24:23 +0530

    Added Login & Registration Form

--- authenticate/urls.py
+++ authenticate/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.login),
-    path('signup/',views.signup),
+    path('',views.login,name="login"),
+    path('signup/',views.signup,name="signup"),
 ]
